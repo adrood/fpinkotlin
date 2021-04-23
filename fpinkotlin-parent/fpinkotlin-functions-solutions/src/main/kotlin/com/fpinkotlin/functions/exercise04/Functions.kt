3e9b11530080736c72e6fdec4c80dfb6edbdd461
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-04-23 09:40:20 +0200

    Used standard naming for functions.

--- fpinkotlin-parent/fpinkotlin-functions-solutions/src/main/kotlin/com/fpinkotlin/functions/exercise04/Functions.kt
+++ fpinkotlin-parent/fpinkotlin-functions-solutions/src/main/kotlin/com/fpinkotlin/functions/exercise04/Functions.kt
@@ -9,7 +9,7 @@ fun <T, U, V> compose(f: (U) -> V, g: (T) -> U): (T) -> V = { f(g(it)) }
 
 val add: (Int) -> (Int) -> Int = { a -> { b -> a + b} }
 
-val compose = { x: (Int) -> Int -> { y: (Int) -> Int -> { z: Int -> x(y(z)) } } }
+val compose = { f: (Int) -> Int -> { g: (Int) -> Int -> { z: Int -> f(g(z)) } } }
 
 class Type<out T>
 
