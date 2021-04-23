f76dcc68285ff291a1175b7636cda32ce61d20cb
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-04-23 09:37:44 +0200

    Fixed compile errors by commenting out some code.

--- fpinkotlin-parent/fpinkotlin-recursion-solutions/src/main/kotlin/com/fpinkotlin/recursion/exercise03/Fibonacci.kt
+++ fpinkotlin-parent/fpinkotlin-recursion-solutions/src/main/kotlin/com/fpinkotlin/recursion/exercise03/Fibonacci.kt
@@ -9,10 +9,13 @@ fun fibonacci(number: Int): Int =
             fibonacci(number - 1) + fibonacci(number - 2)
 
 
+// Error: The sequence starts with the wrong number: fib(0) should be 0
 fun fib(x: Int): BigInteger {
     tailrec
     fun fib(val1: BigInteger, val2: BigInteger, x: BigInteger): BigInteger = when {
         (x == BigInteger.ZERO) -> BigInteger.ONE
+        // The recursion should continue one step further
+        // There is no need to perform the addition explicitly
         (x == BigInteger.ONE) -> val1 + val2
         else -> fib(val2, val1 + val2, x - BigInteger.ONE)
     }
