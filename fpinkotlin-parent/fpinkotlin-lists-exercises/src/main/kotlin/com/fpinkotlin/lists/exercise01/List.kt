70baa99cd2916925a5c3a75f08b7ac3fdec9452b
Ad Roodhuijzen <ad.roodhuijzen@gmail.com>
2021-04-23 09:41:38 +0200

    Added comments containing remarks.

--- fpinkotlin-parent/fpinkotlin-lists-exercises/src/main/kotlin/com/fpinkotlin/lists/exercise01/List.kt
+++ fpinkotlin-parent/fpinkotlin-lists-exercises/src/main/kotlin/com/fpinkotlin/lists/exercise01/List.kt
@@ -1,18 +1,24 @@
 package com.fpinkotlin.lists.exercise01
 
-sealed class List<A> {
+// out A (if not used, the function of doesn't compile
+sealed class List<out A> {
 
     abstract fun isEmpty(): Boolean
 
-    fun cons(a: A): List<A> = TODO("cons")
+    // Not needed, we construct instances of List with the idiomatic 'of' method
+    // Type parameter A occurs in the in-position
+//    fun cons(a: A): List<A> = //TODO("cons")
+//            Cons(a, this)
 
-    private object Nil: List<Nothing>() {
+    // Why private?
+    private object Nil : List<Nothing>() {
 
         override fun isEmpty() = true
 
         override fun toString(): String = "[NIL]"
     }
 
+    // data class, why private?
     private class Cons<A>(internal val head: A, internal val tail: List<A>) : List<A>() {
 
         override fun isEmpty() = false
@@ -26,7 +32,14 @@ sealed class List<A> {
     }
 
     companion object {
+        //        For data types, it is a common idiom to have a variadic of method in the
+//        companion object to conveniently construct instances of the data type.
+        fun <A> of(vararg aa: A): List<A> {
+            val tail = aa.sliceArray(1 until aa.size)
+            return if (aa.isEmpty()) Nil else Cons(aa[0], List.of(*tail))
+        }
 
+        //
         operator fun <A> invoke(vararg az: A): List<A> =
                 az.foldRight(Nil as List<A>) { a: A, list: List<A> -> Cons(a, list) }
     }
